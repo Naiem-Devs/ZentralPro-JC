f8796accc11ae46fed1f9332101442784f5bd7e1
Naiem-Devs <dnsnaiem@gmail.com>
2023-04-20 11:38:06 +0600

    ad aos

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,29 +31,11 @@
     }
   });
 
-  // owlCarousel
-  $(".brand-active").owlCarousel({
-    loop: true,
-    margin: 30,
-    items: 6,
-    navText: [
-      '<i class="fa fa-angle-left"></i>',
-      '<i class="fa fa-angle-right"></i>'
-    ],
-    nav: false,
-    dots: false,
-    responsive: {
-      0: {
-        items: 2
-      },
-      767: {
-        items: 3
-      },
-      992: {
-        items: 6
-      }
-    }
+  AOS.init({
+    mirror: true,
+    duration: 1500,
+    initClassName: 'aos-init',
+    once: true,
   });
-
  
 })(jQuery);
