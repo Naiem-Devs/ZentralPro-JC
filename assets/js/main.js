6ffcedffd7489c6dc224c1f40cefd2a074c72296
Naiem-Devs <dnsnaiem@gmail.com>
2023-04-20 11:12:02 +0600

    upted file

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,32 @@
  // menu 
   $('.siteBar-btn').click( function (){ 
     $('.mobile-menu').toggleClass('siteBar');   
+    $(".siteBar-btn i").toggleClass('fa-times');   
   }); 
 
+
+  // owlCarousel
+  $(".Integration_slider").owlCarousel({
+    loop: true,
+    margin: 30,
+    navText: [
+      '<i class=""></i>',
+      '<img src="assets/img/Integration/right.svg" alt="">'
+    ],
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      767: {
+        items: 3
+      },
+      992: {
+        items: 4
+      }
+    }
+  });
 
   // owlCarousel
   $(".brand-active").owlCarousel({
